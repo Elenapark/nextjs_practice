c2dfe721b2a820ac8548fb41e1abd972800cbfd6
elenapark <elenability@naver.com>
2021-08-13 14:58:09 -0700

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import HeadInfo from "../components/HeadInfo";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,4 +9,5 @@ export default function Home() {
     </div>
   )
 }
+
 
