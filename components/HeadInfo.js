fd23b0b007a5ba352cb8e795af3a25ed75bf91da
simplogis <simplogis@simplogiss-MacBook-Air.local>
2021-08-13 14:47:33 -0700

    first commit: next.js practice

---
+++ components/HeadInfo.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const HeadInfo = ({title, keyword, contents}) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta keyword={keyword} />
+      <meta contents={contents} />
+    </Head>
+  );
+};
+
+HeadInfo.defaultProps ={
+  title: 'My Blog',
+  keyword : 'Blog powered by Next js',
+  contents: 'Practice next js'
+}
+export default HeadInfo;
